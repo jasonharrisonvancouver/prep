55154c8a8e661ae7853c95dd92d01f4b755556af
Jason Harrison <jasonharrisonvancouver@gmail.com>
2018-12-28 16:11:45 -0800

    get user input and its length
    "Interacting with Users" module

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  FizzBussJasonHarrison
+//  User String Length Calculator
 //
 //  Created by jason harrison on 2018-12-28.
 //  Copyright © 2018 jason harrison. All rights reserved.
@@ -10,20 +10,14 @@
 
 int main(int argc, const char * argv[]) {
     
-    for(int i = 1; i <= 100; i++){
-        if(((i % 3) == 0) && (i % 5 == 0)){
-            printf("FizzBuzz");
-            
-        }else if((i%3) == 0){
-            printf("Fizz");
-        }else if((i%5) ==0){
-            printf("Buzz");
-        }else{
-            printf("%d", i);
-        }
-        
-    }
+    
+    char input[128];
+    
+    printf("Type something and press return: ");
+    scanf("%s", input);
+    printf("\nYou typed: %s, which has %d characters!", input, strlen(input));
     
     
+
     return 0;
 }
